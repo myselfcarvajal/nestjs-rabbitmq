6c970a4fea244b9892c936b012b8f46a82018504
myselfcarvajal <myselfcarvajal@gmail.com>
2025-02-08 09:02:41 -0500

    add environment template

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProducerModule } from './producer/producer.module';
 import { ConsumerModule } from './consumer/consumer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProducerModule, ConsumerModule],
+  imports: [ConfigModule.forRoot(), ProducerModule, ConsumerModule],
   controllers: [AppController],
   providers: [],
 })
